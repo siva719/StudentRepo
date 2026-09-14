9b25c2217e9a02fc9653b79ba0a9682f7fa44377
siva neelapala <sivaneelapala994@gmail.com>
2026-09-14 16:30:20 +0530

    main

--- src/main/java/com/example/student_service/StudentServiceApplication.java
+++ src/main/java/com/example/student_service/StudentServiceApplication.java
@@ -8,6 +8,7 @@ public class StudentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServiceApplication.class, args);
+		System.out.println("Hello from MAIN");
 	}
 
 }
